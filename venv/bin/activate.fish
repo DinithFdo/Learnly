8062af5714f23a83fed9eb993e08038b81b1a723
DinithFdo <dinithofficial01@gmail.com>
2023-04-10 22:18:36 +0530

    Created new venv

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/dinithfernando/Documents/Projects/SDGP_SE-75/venv"
+set -gx VIRTUAL_ENV "/Users/dinithfernando/Documents/Projects/Learnly/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
